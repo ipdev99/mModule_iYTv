019934e73fcb8658686ba6ee514c5048a1d9c84a
ip <ipdev99@gmail.com>
2021-01-27 20:38:54 -0600

    service
    Update default service script to YouTube v16.02.35

--- service.sh
+++ service.sh
@@ -4,7 +4,7 @@
 ## ipdev @ xda-developers
 
 ## Requires YouTube to be installed as a user app.
-## Required YouTube version is 15.43.32
+## Required YouTube version is 16.02.35
 
 # Module directory set by Magisk.
 MODDIR=${0%/*}
@@ -13,7 +13,7 @@ MODDIR=${0%/*}
 RUNMOUNT=TRUE
 
 # Required YT versionCode.
-RVCODE=1515701696
+RVCODE=1518722496
 
 # Get SDK level and architecture.
 SDK=$(getprop ro.build.version.sdk);
